7e19c88897305e0ab3359e9a307310a369d2824b
Bala Bhadra Maharjan <bmaharjna@lftechnology.com>
2015-07-04 09:07:53 +0545

    Start line number from 1

--- lib/debug_sniffer/issue_detector.rb
+++ lib/debug_sniffer/issue_detector.rb
@@ -2,10 +2,10 @@ module DebugSniffer
   module IssueDetector
     
     def detect_issue(description)
-      File.open(@file).each_with_index do |line, index|
+      File.open(@file).each.with_index(1) do |line, index|
         @terms.each do |term|
           if line =~ /#{term}/
-            issue = {"type"=> "issue", "check_name" => "Debug codes", "description" => description, "categories" => ["Clarity", "Style"], "location" => {"path" => @file, "lines" => {"begin"  => index, "end" => index}}, "remediation_points" => 500}
+            issue = {"type"=> "issue", "check_name" => "Debug codes", "description" => description, "categories" => ["Clarity", "Style"], "location" => {"path" => @file, "lines" => {"begin"  => index}}, "remediation_points" => 500}
             STDOUT.print "#{issue.to_json}\0"
             break
           end
