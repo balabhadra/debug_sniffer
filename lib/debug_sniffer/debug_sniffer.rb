e3eac03ea486faa8027ddf535652dacefe7d9436
Shiva Bhusal <cba.bhusal@outlook.com>
2015-07-04 11:15:29 +0545

    Doc added in some files

--- lib/debug_sniffer/debug_sniffer.rb
+++ lib/debug_sniffer/debug_sniffer.rb
@@ -3,22 +3,22 @@ require 'debug_sniffer/parser'
 
 module DebugSniffer
   class DebugSniffer
-
+    # Constructor
     # @param directory [String] filepath
-    # @param engine_config [hash] excludes the file
-
+    # @param engine_config [Hash] excludes the file
     def initialize(directory, engine_config)
       file_finder = FileFinder.new(engine_config)
       @files = file_finder.files(directory)
       @parser = Parser.new(directory)
     end
 
+    # Fed the files one by one to the Parser Instance
+    # @return [Boolean]
     def run
       @files.each do |file|
         @parser.parse(file)
       end
     end
-
   end
 end
 
--- lib/debug_sniffer/file_finder.rb
+++ lib/debug_sniffer/file_finder.rb
@@ -1,14 +1,21 @@
 require 'yaml'
 
 module DebugSniffer
+  # Searches for files supported in relevant directory
   class FileFinder
-  
+
+    # Constructor
+    # @param engine_config [Hash] excludes the file
     def initialize(engine_config)
       @engine_config = engine_config
       file_config = YAML.load_file("config/config.yml")
       @relevant_type = file_config['debug_sniffer']['extensions'].keys
     end
 
+    # Gets all the files supported in relevant directory
+    #   along with excludes the files (path) set in .codeclimate.yml
+    # @param directory [String] path to files
+    # @return [Array] collection of Files (String)
     def files(directory)
       Dir.chdir(directory)
       files = []
@@ -20,7 +27,9 @@ module DebugSniffer
     end
 
     private
-    
+
+    # Excludes files (path) set in config of .codeclimate.yml file
+    # @param files [Array] collection of Files (String)
     def exclude_files(files)
       exclusions = @engine_config['exclude_paths'] || []
       files.reject { |f| exclusions.include?(f) }
